3b58c326d2ab2945033249f5cd4bcff52b16d6e0
Luis Monasterios <ljmm2k2@gmail.com>
2024-03-12 15:01:21 -0400

    first stable version

--- funct.js
+++ funct.js
@@ -1,11 +1,8 @@
 let container = document.querySelector(".container");
 let cuadro = document.createElement("div");
 cuadro.classList.add("square");
-
 cuadro.setAttribute("style", 
-" width: 5.95%; height: 5.95%;  border: 1.1px; border-style: solid; border-color: gray;");
-
-
+" width: 5.954%; height: 5.954%;  border: 1.1px; border-style: solid; border-color: gray;");
 // cuadro.setAttribute("style", 
 // "flex-wrap: wrap, width: 3%; margin-inline: 0.05%; height: 3%;  border: 1px; border-style: solid; border-color: black;");
 for (let i=0; i<256;i++){
@@ -19,4 +16,3 @@ let cuadrosdiv = document.querySelectorAll(".square");
 cuadrosdiv.forEach(div => {
     div.addEventListener("mouseover", BgColor);
 });
-
