e7804808399129f8e6976dea52bddd3162496add
Luis Monasterios <ljmm2k2@gmail.com>
2024-03-23 09:59:33 -0400

    functional etch-a-sketch

--- funct.js
+++ funct.js
@@ -1,13 +1,23 @@
 let container = document.querySelector(".container");
+let spaned = document.createElement("span");
 let cuadro = document.createElement("div");
 cuadro.classList.add("square");
+spaned.classList.add("espan");
+spaned.setAttribute("style","border: 1.1px; border-style: solid border-color: lightgray; flex: 1; display:flex;");
 cuadro.setAttribute("style", 
-" width: 5.954%; height: 5.954%;  border: 1.1px; border-style: solid; border-color: gray;");
+"flex-grow:1; border: 1.1px; border-style: solid; border-color: lightgray;");
+for (let i = 0; i < 100; i++) {
+    let newSpaned = spaned.cloneNode(true);
+    container.appendChild(newSpaned);
+    for (let j = 0; j < 100; j++) {
+        newSpaned.appendChild(cuadro.cloneNode(true));
+    }
+}
 // cuadro.setAttribute("style", 
 // "flex-wrap: wrap, width: 3%; margin-inline: 0.05%; height: 3%;  border: 1px; border-style: solid; border-color: black;");
-for (let i=0; i<256;i++){
-    container.appendChild(cuadro.cloneNode(true));   
-}
+// for (let i=0; i<256;i++){
+//     container.appendChild(cuadro.cloneNode(true));   
+// }
 function BgColor(){
     this.classList.add("black");
 }
